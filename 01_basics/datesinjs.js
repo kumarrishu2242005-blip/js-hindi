4d291b8aaf6afaae983865821d21be4e26a70af9
kumarrishu2242005-blip <kumarrishu2242005@gmail.com>
2026-06-25 16:44:17 +0000

    dateinjs

---
+++ 01_basics/datesinjs.js
@@ -0,0 +1,35 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());  // Thu Jun 25 2026 16:09:03 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toDateString()); // Thu Jun 25 2026
+// console.log(myDate.toLocaleString()); // 6/25/2026, 4:10:20 PM
+// console.log(typeof myDate);  //object
+
+//  let myCreatedDate = new Date(2023, 0, 23)  // 0 => jan (month start with zero index)
+//  console.log(myCreatedDate.toDateString())  // Mon Jan 23 2023
+
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// console.log(myCreatedDate.toLocaleString());  // 1/23/2023, 5:03:00 AM
+
+// // let myCreatedDate = new Date("2023-01-14")
+// let myCreatedDate = new Date("01-14-2023")
+// // console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp = Date.now()
+
+// // console.log(myTimeStamp);
+// // console.log(myCreatedDate.getTime());
+// // console.log(Math.floor(Date.now()/1000));
+
+// let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+// // `${newDate.getDay()} and the time `
+
+// newDate.toLocaleString('default', {
+//     weekday: "long",
+    
+// })
